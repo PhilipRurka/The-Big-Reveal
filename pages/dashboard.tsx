0b176687845ad13a7f1a8966079b3c70de8bded9
Philip Rurka <hey@philiprurka.com>
2022-11-17 09:30:38 -0400

    Redirecting login to the dashboard

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,17 +1,29 @@
-import { getSession, withPageAuthRequired } from "@auth0/nextjs-auth0";
+import { getSession, useUser, withPageAuthRequired } from "@auth0/nextjs-auth0";
 import { initialConsole } from "./api/initial-console";
 
 const Dashboard = () => {
-  return (
-    <main>
-      <h1>Dashboard</h1>
-    </main>
-  );
+  const {
+    isLoading,
+    error,
+    user
+  } = useUser()
+
+  if(isLoading) { <div>... Is loading</div> }
+  else if(error) { console.error(error); return <></> }
+  else if(user) {
+    return (
+      <main>
+        <h1>Dashboard</h1>
+      </main>
+    );
+  }
+
 }
 
 export const getServerSideProps = withPageAuthRequired({
   async getServerSideProps(context) {
     initialConsole('Login')
+
     const session = getSession(context.req, context.res);
     console.log(session)
     return {
