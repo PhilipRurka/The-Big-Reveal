7464e6375f170c2625e0f8e8bef11ea248813b9e
Philip Rurka <hey@philiprurka.com>
2023-03-07 16:09:40 -0500

    Moving Reset Password logic and adjusting logic

--- src/components/resetPassword/ResetPassword.container.tsx
+++ src/components/resetPassword/ResetPassword.container.tsx
@@ -1,7 +1,56 @@
-import React from 'react'
+import { AuthChangeEvent } from '@supabase/supabase-js'
+import Router from 'next/router'
+import React, { useEffect } from 'react'
+import { useAppDispatch } from '../../redux/redux_hooks'
+import { update_userData } from '../../redux/slices/userSlice'
+import { supabase } from '../../utils/supabase'
 import ResetPassword from './ResetPassword'
 
 const ResetPasswordContainer = () => {
+  const dispatch = useAppDispatch()
+
+  useEffect(() => {
+    const { data: authListener } = supabase.auth.onAuthStateChange(async (event, session) => {
+      if (event === "PASSWORD_RECOVERY") {
+        console.log({
+          event,
+          session
+        })
+
+        const newPassword = prompt("What would you like your new password to be?")
+
+        const {
+          data,
+          error
+        } = await supabase.auth.updateUser({ password: newPassword || undefined })
+
+        console.log({
+          data,
+          error
+        })
+
+      } else {
+        if(event === "SIGNED_IN") {
+          console.log({
+            event,
+            session
+          })
+          if(session) {
+            dispatch(update_userData(session))
+
+            Router.push('dashboard')
+          }
+        }
+      }
+    })
+
+    return () => {
+      if (authListener?.subscription) {
+        authListener.subscription.unsubscribe()
+      }
+    };
+  }, [supabase.auth])
+
   return <ResetPassword />
 }
 
