fdc089cb3f60761c28e29e07368138cdcff40b8a
Philip Rurka <hey@philiprurka.com>
2023-03-30 11:03:08 -0400

    Messy - transformed hoc from Function Component to Class Component

--- src/hoc/withAuthRequired.tsx
+++ src/hoc/withAuthRequired.tsx
@@ -1,11 +1,11 @@
 import Router from "next/router"
-import { useEffect } from "react"
+import React, { useEffect } from "react"
 import { supabase } from "../utils/supabase"
 
-// eslint-disable-next-line react/display-name
-const withAuthRequired = (Component: any) => (props: any) => {
-  useEffect(() => {
-    (async () => {
+function withAuthRequired(Component: any) {
+  return class extends React.Component {
+
+    async componentDidMount(): Promise<void> {
       const { data, error } = await supabase.auth.getSession()
 
       if(error) {
@@ -15,10 +15,33 @@ const withAuthRequired = (Component: any) => (props: any) => {
       if(!data.session) {
         Router.push('auth')
       }
-    })()
-  }, [])
+    }
 
-  return <Component {...props}/>
+    render() {
+      return <Component {...this.props} />
+    }
+  }
 }
+
+
+
+
+// const withAuthRequired = (Component: any) => (props: any) => {
+//   useEffect(() => {
+//     (async () => {
+//       const { data, error } = await supabase.auth.getSession()
+
+//       if(error) {
+//         console.error(error)
+//       }
+
+//       if(!data.session) {
+//         Router.push('auth')
+//       }
+//     })()
+//   }, [])
+
+//   return <Component {...props}/>
+// }
 
 export default withAuthRequired
