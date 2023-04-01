6521c0bf124b2a848cd185bfe59876e9eaabdb74
Philip Rurka <hey@philiprurka.com>
2023-04-01 13:11:11 -0400

    Oops

--- src/utils/navigation.ts
+++ src/utils/navigation.ts
@@ -10,7 +10,7 @@ export const navWithoutAuth: NavigationsType = [
   }
 ]
 
-export const navWithAuth: NavigationsType = [=
+export const navWithAuth: NavigationsType = [
   {
     name: 'Dashboard',
     path: '/dashboard'
