e62438fbf5f54172b2c62c61ecae7f22204ff0bf
Philip Rurka <hey@philiprurka.com>
2023-04-14 12:30:16 -0400

    Schema - Added trigger and function for Profile creation

---
+++ supabase/migrations/20230414162924_create_trigger_function.sql
@@ -0,0 +1,19 @@
+CREATE TRIGGER on_auth_user_created AFTER INSERT ON auth.users FOR EACH ROW EXECUTE FUNCTION handle_new_user();
+
+
+set check_function_bodies = off;
+
+CREATE OR REPLACE FUNCTION public.handle_new_user()
+ RETURNS trigger
+ LANGUAGE plpgsql
+ SECURITY DEFINER
+AS $function$
+begin
+  insert into public.profiles (id, full_name, avatar_url)
+  values (new.id, new.raw_user_meta_data->>'full_name', new.raw_user_meta_data->>'avatar_url');
+  return new;
+end;
+$function$
+;
+
+
