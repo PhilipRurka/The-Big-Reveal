841ad65413c617799edfc7b9f81a4ccbd5efd3b1
Philip Rurka <hey@philiprurka.com>
2023-04-17 11:29:49 -0400

    Created the NewComponentTemplate and updated the README

---
+++ NewComponentTemplate/NewComponentTemplate.tsx
@@ -0,0 +1,12 @@
+import { FC } from "react";
+import { NewComponentTemplateWrapper } from "./NewComponentTemplate.styled";
+
+const NewComponentTemplate: FC = () => {
+  return (
+    <NewComponentTemplateWrapper>
+      
+    </NewComponentTemplateWrapper>
+  )
+}
+
+export default NewComponentTemplate
