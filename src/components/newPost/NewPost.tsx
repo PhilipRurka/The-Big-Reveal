eaa61189eef6e41a7d2ab3dda22ee7e70203a95a
Philip Rurka <hey@philiprurka.com>
2023-04-19 16:31:47 -0400

    Added more to the seed

--- src/components/newPost/NewPost.tsx
+++ src/components/newPost/NewPost.tsx
@@ -3,6 +3,7 @@ import { Form, NewPostWrapper,  Title } from "./NewPost.styled"
 import { Button, ButtonWrapper } from "../../styled/button"
 import Tiny from "../tiny"
 import { Editor } from "tinymce"
+import { Label } from "../../styled"
 
 type NewPostType = {
   handleSubmit: (event: FormEvent) => void
@@ -21,7 +22,14 @@ const NewPost: FC<NewPostType> = ({
         Wanna share something new?
       </Title>
       <Form>
+        <Label>
+          Drop that poem
+        </Label>
         <Tiny tinyRef={poemRef} />
+
+        <Label>
+          The Big Reveal!
+        </Label>
         <Tiny tinyRef={descriptionRef} />
         <ButtonWrapper>
           <Button
