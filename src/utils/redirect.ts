dacdba4939c5191b0e5cbbe8307918f5aacc1356
Philip Rurka <hey@philiprurka.com>
2023-04-23 19:42:19 -0400

    Testing prod again

--- src/utils/redirect.ts
+++ src/utils/redirect.ts
@@ -3,7 +3,8 @@ const includeTrailingSlash = (url: string) => {
 }
 
 export const getRedirectURL = (path: string = '') => {
-  let url = process?.env?.NEXT_PUBLIC_VERCEL_URL ?? 'http://localhost:3000/';
+  // let url = process?.env?.NEXT_PUBLIC_VERCEL_URL ?? 'http://localhost:3000/';
+  let url = 'thebigreveal.club';
     
   url = url.includes('http') ? url : `https://${url}`;
   url = includeTrailingSlash(url)
