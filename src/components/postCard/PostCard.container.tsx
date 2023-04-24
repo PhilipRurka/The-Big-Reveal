30fd406982ec155ebed1cf9c8050e06e7a3cbb86
Philip Rurka <hey@philiprurka.com>
2023-04-24 15:30:27 -0400

    Resolving possible issues with datejs not being in a useEffect

--- src/components/postCard/PostCard.container.tsx
+++ src/components/postCard/PostCard.container.tsx
@@ -1,5 +1,6 @@
-import { FC } from "react"
+import { FC, useEffect, useState } from "react"
 import PostCard from "./PostCard"
+import dayjs from "dayjs"
 
 export type PostCardType = {
   id: string
@@ -10,10 +11,16 @@ export type PostCardType = {
 
 const PostCardContainer: FC<PostCardType> = ({
   id,
-  date,
+  date: rawDate,
   username,
   title
 }) => {
+  const [date, setDate] = useState('')
+
+  useEffect(() => {
+    setDate(dayjs(rawDate).format('D MMM YYYY, h:ss a'))
+  },[])
+
   return (
     <PostCard
       id={id}
