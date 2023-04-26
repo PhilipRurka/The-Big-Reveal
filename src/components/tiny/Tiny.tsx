8f2067f7f8e6afd3d783e2122911c28ffc21bbaf
Philip Rurka <hey@philiprurka.com>
2023-04-26 13:12:44 -0400

    Adjusted Tiny

--- src/components/tiny/Tiny.tsx
+++ src/components/tiny/Tiny.tsx
@@ -1,20 +1,55 @@
 import { FC } from "react";
 import { TinyWrapper } from "./Tiny.styled";
-import { Editor } from "@tinymce/tinymce-react";
+import { Editor as EditorReact } from "@tinymce/tinymce-react";
+import { Fonts } from "../../styled";
+import { Editor } from "tinymce";
 
-type TinyType = any
+type TinyType = {
+  onInit: (_: unknown, editor: Editor) => void
+  tinyId: string,
+  handleChange: (value: string) => void
+}
 
 const Tiny: FC<TinyType> = ({
   onInit,
   tinyId,
-  ...TinyConfigs
+  handleChange
 }) => {
   return (
     <TinyWrapper>
-      <Editor
-        {...TinyConfigs}
+      <EditorReact
         id={tinyId}
-        onInit={onInit} />
+        onInit={onInit}
+        tinymceScriptSrc={`/tinymce/tinymce.min.js`}
+        initialValue=''
+        init={{
+          height: 500,
+          menubar: false,
+          plugins: [
+            'advlist',
+            'autolink',
+            'lists',
+            'link',
+            'image',
+            'charmap',
+            'anchor',
+            'searchreplace',
+            'visualblocks',
+            'code',
+            'fullscreen',
+            'insertdatetime',
+            'media',
+            'table',
+            'preview',
+            'help'
+          ],
+          toolbar: 'undo redo | blocks | ' +
+            'bold italic forecolor | alignleft aligncenter ' +
+            'alignright alignjustify | bullist numlist outdent indent | ' +
+            'removeformat | help',
+          content_style: `body { ${Fonts.tiny} font-size:16px }`,
+        }}
+        onEditorChange={handleChange} />
     </TinyWrapper>
   )
 }
