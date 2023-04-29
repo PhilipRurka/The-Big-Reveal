8623417d974e4873d4f7232cb44b6fc4b66f3688
Philip Rurka <hey@philiprurka.com>
2023-04-29 16:26:48 -0400

    Adjusted logo

--- src/components/header/components/brandLogo/BrandLogo.styled.tsx
+++ src/components/header/components/brandLogo/BrandLogo.styled.tsx
@@ -1,12 +1,12 @@
 import styled from "styled-components"
-import { default as ImageNext } from 'next/image'
+import Image from 'next/image'
 import { default as LinkNext } from 'next/link'
 
 export const BrandLogoWrapper = styled.div``
 
 export const BrandLogoAnchor = styled(LinkNext)``
 
-export const BrandImg = styled(ImageNext)`
+export const BrandImg = styled(Image)`
   height: 30px;
   width: 30px;
   object-fit: contain;
