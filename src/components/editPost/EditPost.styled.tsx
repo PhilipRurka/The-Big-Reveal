386361aa51c1ec419fb38d34d9f7e1ef8192e2ee
Philip Rurka <hey@philiprurka.com>
2023-05-05 14:14:02 -0400

    Edit Post now reaches the far top of the screen

--- src/components/editPost/EditPost.styled.tsx
+++ src/components/editPost/EditPost.styled.tsx
@@ -5,19 +5,20 @@ export const EditPostStyled = styled.div`
 `;
 
 export const Overlay = styled.div`
-  position: absolute;
+  position: fixed;
   top: 0;
   right: 0;
   bottom: 0;
   left: 0;
-  background-color: rgba(0, 0, 0, 0.7);
+  background-color: rgba(0, 0, 0, 0.8);
   opacity: 0;
   cursor: pointer;
+  z-index: 1;
 `;
 
 export const AbsoluteEdit = styled.div`
   position: fixed;
-  top: 73px;
+  top: 0;
   right: 0;
   bottom: 0;
   left: 100px;
@@ -25,4 +26,5 @@ export const AbsoluteEdit = styled.div`
   overflow-y: scroll;
   transform: translateX(100%);
   box-shadow: 0 0 30px -4px black;
+  z-index: 1;
 `;
