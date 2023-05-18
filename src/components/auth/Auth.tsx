c2c5c5f98af0ea143d825249e23d72f2353d52b7
ok-diego <hello.okdiego@gmail.com>
2023-05-18 16:31:35 -0400

    chore: formatting auth component

--- src/components/auth/Auth.tsx
+++ src/components/auth/Auth.tsx
@@ -9,110 +9,112 @@ import {
   FieldContainer,
 } from "./Auth.styled";
 import Input from "../input";
-
 import { Field, Label } from "../../styled";
 import PasswordField from "../passwordField";
 import ConfirmedPasswordField from "../confirmedPasswordField";
-import { FormMessageContainerType } from "../formMessage/FormMessage.container";
 import FormMessage from "../formMessage";
 import EasyLogin from "../easyLogin";
 import { AuthProps, Refs } from "./Auth.type";
 import { AuthTransitionIdsEnum } from "./Auth.enum";
 
-const Auth = forwardRef<Refs, AuthProps>(
-  (
-    {
-      hasEmail,
-      hasPassword,
-      hasUsername,
-      title,
-      handleSubmit,
-      toAuthLinks,
-      password,
-      handlePasswordUpdate,
-      handleConfirmedPasswordUpdate,
-      validationStatuses,
-      disableSubmit,
-      removeStatusMessage,
-      hasPasswordValidation,
-      hasConfirmedPassword,
-      formMessageProps,
-    },
-    { emailRef, passwordRef, usernameRef }: any
-  ) => {
-    return (
-      <AuthStyled>
-        <AuthTitle id={AuthTransitionIdsEnum.TITLE}>{title}</AuthTitle>
-        <Form>
-          <FormMessage {...formMessageProps} />
-          <FieldContainer id={AuthTransitionIdsEnum.USERNAME}>
-            {hasUsername && (
-              <Field>
-                <Label htmlFor="username">Username</Label>
-                <Input
-                  id="username"
-                  type="text"
-                  handleChange={removeStatusMessage}
-                  ref={usernameRef}
-                />
-              </Field>
-            )}
-          </FieldContainer>
-          <FieldContainer id={AuthTransitionIdsEnum.EMAIL}>
-            {hasEmail && (
-              <Field>
-                <Label htmlFor="emailAddress">Email</Label>
-                <Input
-                  id="emailAddress"
-                  type="text"
-                  handleChange={removeStatusMessage}
-                  ref={emailRef}
-                />
-              </Field>
-            )}
-          </FieldContainer>
-          <PasswordField
-            ref={passwordRef}
-            password={password}
-            hasPasswordValidation={hasPasswordValidation}
-            handlePasswordUpdate={handlePasswordUpdate}
-            validationStatuses={validationStatuses}
-            hasPassword={hasPassword}
-          />
-          {hasConfirmedPassword && handleConfirmedPasswordUpdate && (
-            <ConfirmedPasswordField
-              handleUpdate={handleConfirmedPasswordUpdate}
-            />
-          )}
-          {process.env.NODE_ENV === 'development' && (
-              <EasyLogin 
-                emailRef={emailRef} 
-                passwordRef={passwordRef} 
+const Auth = forwardRef<Refs, AuthProps>(({
+  hasEmail,
+  hasPassword,
+  hasUsername,
+  title,
+  handleSubmit,
+  toAuthLinks,
+  password,
+  handlePasswordUpdate,
+  handleConfirmedPasswordUpdate,
+  validationStatuses,
+  disableSubmit,
+  removeStatusMessage,
+  hasPasswordValidation,
+  hasConfirmedPassword,
+  formMessageProps,
+}, { 
+  emailRef, 
+  passwordRef, 
+  usernameRef 
+}: any) => {
+  return (
+    <AuthStyled>
+      <AuthTitle id={AuthTransitionIdsEnum.TITLE}>
+        {title}
+      </AuthTitle>
+      <Form>
+        <FormMessage {...formMessageProps} />
+        <FieldContainer id={AuthTransitionIdsEnum.USERNAME}>
+          {hasUsername && (
+            <Field>
+              <Label htmlFor="username">Username</Label>
+              <Input
+                id="username"
+                type="text"
+                handleChange={removeStatusMessage}
+                ref={usernameRef}
               />
+            </Field>
           )}
-          <SubmitButton
-            type="submit"
-            colorType="primary"
-            onClick={handleSubmit}
-            disabled={disableSubmit}
-          >
-            Submit
-          </SubmitButton>
-        </Form>
-        {toAuthLinks && (
-          <ToAuthLinkStyled id={AuthTransitionIdsEnum.TO_AUTH_LINKS}>
-            {toAuthLinks.map(({ href: toLinkHref, title: toLinkTitle }) => (
-              <ToAuthLinkItem key={toLinkTitle} href={toLinkHref}>
-                {toLinkTitle}
-              </ToAuthLinkItem>
-            ))}
-          </ToAuthLinkStyled>
+        </FieldContainer>
+        <FieldContainer id={AuthTransitionIdsEnum.EMAIL}>
+          {hasEmail && (
+            <Field>
+              <Label htmlFor="emailAddress">
+                Email
+              </Label>
+              <Input
+                id="emailAddress"
+                type="text"
+                handleChange={removeStatusMessage}
+                ref={emailRef} />
+            </Field>
+          )}
+        </FieldContainer>
+        <PasswordField
+          ref={passwordRef}
+          password={password}
+          hasPasswordValidation={hasPasswordValidation}
+          handlePasswordUpdate={handlePasswordUpdate}
+          validationStatuses={validationStatuses}
+          hasPassword={hasPassword} />
+        {hasConfirmedPassword && handleConfirmedPasswordUpdate && (
+          <ConfirmedPasswordField
+            handleUpdate={handleConfirmedPasswordUpdate} />
         )}
-      </AuthStyled>
-    );
-  }
-);
+        {process.env.NODE_ENV === 'development' && (
+          <EasyLogin 
+            emailRef={emailRef} 
+            passwordRef={passwordRef} 
+          />
+        )}
+        <SubmitButton
+          type="submit"
+          colorType="primary"
+          onClick={handleSubmit}
+          disabled={disableSubmit} >
+          Submit
+        </SubmitButton>
+      </Form>
+      {toAuthLinks && (
+        <ToAuthLinkStyled id={AuthTransitionIdsEnum.TO_AUTH_LINKS}>
+          {toAuthLinks.map(({ 
+            href: toLinkHref, 
+            title: toLinkTitle 
+          }) => (
+            <ToAuthLinkItem 
+              key={toLinkTitle} 
+              href={toLinkHref}>
+              { toLinkTitle }
+            </ToAuthLinkItem>
+          ))}
+        </ToAuthLinkStyled>
+      )}
+    </AuthStyled>
+  )
+})
 
 Auth.displayName = "Auth";
 
-export default Auth;
+export default Auth
