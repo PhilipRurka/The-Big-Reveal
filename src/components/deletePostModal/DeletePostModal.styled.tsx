b3a59a1c072ad28b025dd6d73c46947f239e2606
ok-diego <hello.okdiego@gmail.com>
2023-05-31 18:06:59 -0400

    feat: customize delete modal

--- src/components/deletePostModal/DeletePostModal.styled.tsx
+++ src/components/deletePostModal/DeletePostModal.styled.tsx
@@ -9,8 +9,85 @@ export const AbsoluteDeleteWrapper = styled.div`
   top: 50%;
   left: 50%;
   transform: translateX(-50%);
-  background-color: white;
+  color: white;
+  background-color: rgba(35, 35, 35 ,1);
   width: 300px;
   height: 300px;
   z-index: 1;
+  padding: 20px;
 `;
+
+export const DeleteModalHeader = styled.div`
+  height: 50px;
+  background: rgba(40, 40, 40, 1);
+`
+
+export const DeleteButton = styled.button`
+  margin: 20px 0px;
+  cursor: pointer;
+  font-weight: 500;
+  padding: 13px 100px;
+  border-radius: 5px;
+  border: 1px solid rgba(106, 32, 36, 1);
+  border: none;
+  font-size: 0.8rem;
+  color: rgba(199, 81, 86, 1);
+  background: rgba(41, 19, 21, 1);
+  transition: all 0.25s ease;
+`
+
+export const ConfirmForm = styled.form`
+  padding: 25px 0;
+`
+
+export const Input = styled.input`
+  &[type='text'] {
+    font-weight: 500;
+    border-radius: 5px;
+    background: rgba(41, 19, 21, 1);
+    width: 100%;
+    padding: 13px 20px;
+    margin: 8px 0;
+    box-sizing: border-box;
+    color: rgba(199, 81, 86, 1);
+    border: none;
+    border: 1px solid rgba(106, 32, 36, 1);
+    padding: 10px 0;
+    -webkit-transition: 0.5s;
+    transition: 0.5s;
+    outline: none;
+  }
+  input[type='text']:focus {
+    border: 2px solid #555;
+}
+  &[type='submit'] {
+    font-weight: 500;
+    border-radius: 5px;
+    background: rgba(41, 19, 21, 1);
+    width: 100%;
+    padding: 13px 20px;
+    margin: 8px 0;
+    box-sizing: border-box;
+    color: rgba(199, 81, 86, 1);
+    border: none;
+    border: 1px solid rgba(106, 32, 36, 1);
+  }
+  ::placeholder {
+    color: rgba(199, 81, 86, 1);
+    opacity: 1;
+    padding: 10px;
+  }
+  &[type='submit']:disabled {
+    background: rgba(41, 19, 21, 1);
+      border: none;
+  }
+`;
+
+export const CloseModalButton = styled.svg`
+  background-image: url('/assets/svg/circle-x-icon.svg');
+  background-repeat: no-repeat;
+  background-size: contain;
+  width: 40px;
+  height: 40px;
+  color: currentColor;
+`
--- src/components/deletePostModal/DeletePostModal.tsx
+++ src/components/deletePostModal/DeletePostModal.tsx
@@ -1,8 +1,13 @@
-import { FC, RefObject } from "react";
+import { FC, RefObject, SetStateAction, useCallback, useMemo, useState } from "react";
 import { Overlay } from "../../styled/animation";
 import {
   AbsoluteDeleteWrapper,
   DeletePostModalStyled,
+  DeleteButton,
+  DeleteModalHeader,
+  ConfirmForm,
+  Input,
+  CloseModalButton
 } from "./DeletePostModal.styled";
 
 type DeletePostModalType = {
@@ -20,16 +25,59 @@ const DeletePostModal: FC<DeletePostModalType> = ({
   handleDeletePost,
   handleCloseDelete
 }) => {
+
+  const [val, setVal] = useState('')
+
+  // useCallback won't re-render the value 
+  const inputChange = useCallback((event: { target: { value: SetStateAction<string>; }; }) => {
+    setVal(event.target.value);
+  }, [])
+
+  const isDisabled = useMemo(() => {
+    const titleLowerCase = decodedTitle.toLocaleLowerCase()
+    const valLowerCase = val.toLocaleLowerCase()
+
+    return titleLowerCase === valLowerCase ? false : true;
+
+  }, [val, decodedTitle])
+  
+  const click = (event:any) => {
+    event.preventDefault();
+    console.log(val);
+  }
+
+  console.log(isDisabled);
+  
   return (
     <DeletePostModalStyled>
       <Overlay
         ref={overlayRef}
         onClick={handleCloseDelete} />
       <AbsoluteDeleteWrapper ref={absoluteRef}>
-        <h1>{ decodedTitle }</h1>
-        <button onClick={handleDeletePost}>
-          Delete
-        </button>
+          <CloseModalButton 
+            onClick={handleCloseDelete} />
+          <DeleteModalHeader>
+            <h1>Delete Poem</h1>
+          </DeleteModalHeader>
+        <h2>{ decodedTitle }</h2>
+        <ConfirmForm>
+          <label htmlFor='confirm'>
+            Type poem name to confirm
+          </label>
+          <Input 
+            type='text' 
+            id='confirm' 
+            name='confirm' 
+            // placeholder='Type in name of poem'
+            // value={val} 
+            onChange={inputChange}
+            />
+          <DeleteButton
+            disabled={isDisabled}
+            onClick={click} >
+              Delete Poem
+          </DeleteButton>
+        </ConfirmForm>
       </AbsoluteDeleteWrapper>
     </DeletePostModalStyled>
   )
